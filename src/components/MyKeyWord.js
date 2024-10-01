1131a8366bf78be0d1b30d406b6c3fc7c122b9dc
AK-Rasel <raselak101010@gmail.com>
2024-10-01 16:46:41 +0600

    key word Spild

---
+++ src/components/MyKeyWord.js
@@ -0,0 +1,9 @@
+const myKeyWord = {
+  bangladesh: "bangladesh is a small country",
+  world: "hello world",
+  usa: "USA refers to the United States of America, a large country in North America known for its diverse culture, economic power, and global influence.",
+};
+
+const myKeyWordFindKeys = Object.keys(myKeyWord);
+
+export { myKeyWord, myKeyWordFindKeys };
--- src/lib/upload.js
+++ src/lib/upload.js
@@ -4,6 +4,8 @@ import {
   ListObjectsV2Command,
 } from "@aws-sdk/client-s3";
 import OpenAI from "openai";
+import { myKeyWord, myKeyWordFindKeys } from "../components/MyKeyWord";
+
 // ---------------------------------- S3Client ---------------------------
 const s3 = new S3Client({
   region: "auto",
@@ -14,29 +16,6 @@ const s3 = new S3Client({
   },
 });
 
-//----------------------------------- key word------------------------------------------------------------------
-const myKeyWord = {
-  bangladesh: "bangladesh is small country",
-  dhaka: "dhaka is big city",
-  world: "hello world",
-  usa: "usa is a large country",
-  london: "london is a historic city",
-  java: "java is a programming language",
-  react: "react is a JavaScript library",
-  india: "india is known for its diversity",
-  paris: "paris is the city of love",
-  python: "python is a versatile programming language",
-  apple: "apple makes popular tech products",
-  amazon: "amazon is a leading e-commerce company",
-  nile: "the nile is the longest river",
-  everest: "mount everest is the highest peak",
-  sun: "the sun is a star",
-  moon: "the moon orbits the earth",
-  galaxy: "our galaxy is the Milky Way",
-  europe: "europe is a continent",
-  africa: "africa has diverse wildlife",
-};
-const myKeyWordFindKeys = Object.keys(myKeyWord);
 // ------------------------------------ OpenAI ------------------------------------------
 const openai = new OpenAI({
   apiKey: import.meta.env.VITE_OPENAI_API_KEY,
@@ -75,8 +54,8 @@ export async function generateAIVoice(file) {
 }
 
 export async function uploadAIVoice(file) {
-  const [a, b] = file;
-  const blob = await b.blob();
+  const [keyName, audioFile] = file;
+  const blob = await audioFile.blob();
 
   const audio = new File([blob], "audio.mp3", { type: blob.type });
   const params = {
@@ -93,7 +72,7 @@ export async function uploadAIVoice(file) {
         console.log(myKeyWordFindKeys);
         const command = new PutObjectCommand({
           Bucket: import.meta.env.VITE_R2_BUCKET_NAME,
-          Key: a + ".mp3",
+          Key: keyName + ".mp3",
           Body: audio,
           ContentType: "audio/mpeg",
         });
