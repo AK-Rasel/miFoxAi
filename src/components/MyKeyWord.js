3922c2ca846d16bfa3e5311dc838146af327c34d
AK-Rasel <raselak101010@gmail.com>
2024-10-01 17:46:36 +0600

    no emtiry cloudflare problem fix1

--- src/components/MyKeyWord.js
+++ src/components/MyKeyWord.js
@@ -2,6 +2,7 @@ const myKeyWord = {
   bangladesh: "bangladesh is a small country",
   world: "hello world",
   usa: "USA refers to the United States of America, a large country in North America known for its diverse culture, economic power, and global influence.",
+  hello: "hello world",
 };
 
 const myKeyWordFindKeys = Object.keys(myKeyWord);
